a695b4d8632c00d5a8456c3f5506055453f7949d
Bhushan Sonawane <bhushan.bookmeds@outlook.com>
2019-09-30 16:30:27 +0530

    edited project model

--- NonProfitCRM/Models/Project.cs
+++ NonProfitCRM/Models/Project.cs
@@ -10,8 +10,6 @@ namespace NonProfitCRM.Models
         public int Id { get; set; }
         public string OrgId { get; set; }
         public string Name { get; set; }
-        public int? CategoryId { get; set; }
-        public int? CampaignId { get; set; }
         public DateTime? Date { get; set; }
         public decimal? Expenses { get; set; }
         public string AddressLine1 { get; set; }
