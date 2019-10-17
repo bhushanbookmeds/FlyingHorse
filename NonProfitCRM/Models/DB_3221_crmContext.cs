0b4d960ccf3410d645edabb6c6758b0074d09eff
NOMAAN ALI <ali.nomaan7@gmail.com>
2019-10-17 13:34:46 +0530

    update Donation Module

--- NonProfitCRM/Models/DB_3221_crmContext.cs
+++ NonProfitCRM/Models/DB_3221_crmContext.cs
@@ -36,8 +36,8 @@ namespace NonProfitCRM.Models
             if (!optionsBuilder.IsConfigured)
             {
 
-                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=tcp:s08.everleap.com;Persist Security Info=True;User ID=DB_3221_crm_user;Password=summi786");
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-SV6VE0R\SQLEXPRESS;Database=NonProfitCRM;Trusted_Connection=True");
             }
         }
 
