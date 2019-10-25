4d76426825e2ab5461e2b3cbb6ffabb0aa2b992f
Sominathwagh456 <56534111+Sominathwagh456@users.noreply.github.com>
2019-10-25 12:21:50 +0530

    Update Contact.cs

--- NonProfitCRM/Models/Contact.cs
+++ NonProfitCRM/Models/Contact.cs
@@ -72,8 +72,7 @@ namespace NonProfitCRM.Models
         public string Gender { get; set; }
         public string ImagePath { get; set; }
 
-        public string Country { get; set; }
-
+  
         public ContactType ContactType { get; set; }
         public ICollection<Project> Project { get; set; }
         public ICollection<Donation> Donation { get; set; }
